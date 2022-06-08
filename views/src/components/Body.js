3e0fbd55a940e22110087db1e9727491c777b4ff
Ponmile Adebisi <ponmileadebisi@gmail.com>
2022-06-08 17:31:33 +0100

    body design

--- views/src/components/Body.js
+++ views/src/components/Body.js
@@ -6,13 +6,22 @@ const Body = () => {
       <div className="container mt-5">
         <div className="card mt-3">
           <div className="card-body">
-            <h5 className="card-title">Blog Titile</h5>
+            <h5 className="card-title mt-2">Blog Titile</h5>
+          </div>
+          <div className="card-body">
             <p className="card-text">Blog Content</p>
           </div>
-          <textarea cols="30" row="10" className="mt-3 form-control"></textarea>
-          <Link to="" className="btn btn-primary">
-            Comment
-          </Link>
+          <div className="card-body">
+            <textarea
+              cols="10"
+              row="10"
+              placeholder="Leave a comment"
+              className="mt-3 form-control"
+            ></textarea>
+            <Link to="" className="btn btn-primary mt-3">
+              Comment
+            </Link>
+          </div>
         </div>
       </div>
     </>
--- views/src/components/blogpost/PostBlog.js
+++ views/src/components/blogpost/PostBlog.js
@@ -6,9 +6,9 @@ const PostBlog = () => {
         <div class="container mt-5">
           <h3 class="my-3">Compose Your Blog Here...</h3>
           <form>
-          <input type="text" name="title" class="form-control" placeholder="Enter title here..."></input>
+          <input type="text" name="title" class="form-control" placeholder="Enter title here..." required></input>
           <textarea name="content" cols="30" row="10" class="mt-3 form-control" placeholder="Enter content here" required></textarea>
-          <input type="text" name="athur" class="mt-3 form-control" placeholder="Name"></input>
+          <input type="text" name="athur" class="mt-3 form-control" placeholder="Name" required></input>
           <input type="submit" value="Compose" class="mt-4 btn btn-md btn-danger"></input>
           </form>
         </div>
